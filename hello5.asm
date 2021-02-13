5823be12bdc54e5ab689057050359f69a7802473
Andrei <margert95@gmail.com>
2021-02-13 15:11:10 +0000

    add comments

--- hello5.asm
+++ hello5.asm
@@ -1,12 +1,12 @@
-%include "stud_io.inc"
+%include "stud_io.inc"  ; includes macrodirectives to print chars
 global _start
 
-section .text
-_start:	mov	eax, 0
- again:	PRINT 	"Hello"
-    	PUTCHAR	10
-    	inc 	eax
-    	cmp	eax, 5
-    	jl	again
-    	FINISH
+section .text           ; executable code section
+_start:	mov	    eax, 0  ; put zero in eax as a counter variable
+ again:	PRINT 	"Hello" ; call marcos to print string
+    	PUTCHAR	10      ; put char with code 10 (new line)
+    	inc 	eax     ; increment value at eax by 1
+    	cmp	    eax, 5  ; compare eax to 5
+    	jl	    again   ; "jump if lower" to "again" label above
+    	FINISH          ; stop execution macros
 
