40dfa18e34433b6c97f9e18130d65286db4b12dd
Andrei <margert95@gmail.com>
2021-03-20 14:51:08 +0000

    Align comments

--- hello5.asm
+++ hello5.asm
@@ -1,12 +1,12 @@
-%include "stud_io.inc"  ; includes macrodirectives to print chars
+%include "stud_io.inc"      ; includes macrodirectives to print chars
 global _start
 
-section .text           ; executable code section
-_start:	mov	    eax, 0  ; put zero in eax as a counter variable
- again:	PRINT 	"Hello"  ; call marcos to print string
-    	PUTCHAR	10         ; put char with code 10 (new line)
-    	inc 	eax           ; increment value at eax by 1
-    	cmp	    eax, 5     ; compare eax to 5
+section .text               ; executable code section
+_start:	mov	    eax, 0      ; put zero in eax as a counter variable
+ again:	PRINT 	"Hello"     ; call marcos to print string
+    	PUTCHAR	10          ; put char with code 10 (new line)
+    	inc 	eax         ; increment value at eax by 1
+    	cmp	    eax, 5      ; compare eax to 5
     	jl	    again       ; "jump if lower" to "again" label above
-    	FINISH             ; stop execution macros
+    	FINISH              ; stop execution macros
 
---
+++ match_pattern.asm
@@ -0,0 +1,8 @@
+; this snippet compares the reference string with an example string
+; character '?' matches with any other character of the reference string;
+; character '*' matches with any substring of the reference string
+; even an empty substring;
+
+global _start
+
+match:  
