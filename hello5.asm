5e55ada555592c042260c0451fc5f6859144c0c1
Andrei <margert95@gmail.com>
2021-03-20 14:53:27 +0000

    Align comments

--- hello5.asm
+++ hello5.asm
@@ -2,11 +2,11 @@
 global _start
 
 section .text               ; executable code section
-_start:	mov	    eax, 0      ; put zero in eax as a counter variable
- again:	PRINT 	"Hello"     ; call marcos to print string
-    	PUTCHAR	10          ; put char with code 10 (new line)
-    	inc 	eax         ; increment value at eax by 1
-    	cmp	    eax, 5      ; compare eax to 5
-    	jl	    again       ; "jump if lower" to "again" label above
+_start:	mov     eax, 0      ; put zero in eax as a counter variable
+ again:	PRINT   "Hello"     ; call marcos to print string
+    	PUTCHAR 10          ; put char with code 10 (new line)
+    	inc     eax         ; increment value at eax by 1
+    	cmp     eax, 5      ; compare eax to 5
+    	jl      again       ; "jump if lower" to "again" label above
     	FINISH              ; stop execution macros
 
